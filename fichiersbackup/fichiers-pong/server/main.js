b909f877956dd2dfcde6e7f79bbd81ffeaad8308
Alexandre <alexandre.ledun.etu@univ-lille.fr>
2022-01-27 13:40:45 +0100

    V0 et V1 de pong DONE

--- fichiersbackup/fichiers-pong/server/main.js
+++
@@ -1,8 +0,0 @@
-import http from 'http';
-import RequestController from './controllers/requestController.js';
-
-const server = http.createServer(
-	(request, response) => new RequestController(request, response).handleRequest()
-);
-
-server.listen(8080);
