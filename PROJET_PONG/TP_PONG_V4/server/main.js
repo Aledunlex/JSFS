ba4e4df02d7d2122f1b72aa242f788014bfbcd6e
Alexandre <alexandre.ledun.etu@univ-lille.fr>
2022-02-23 10:10:09 +0100

    number of players handled server-side; client-side todo

--- PROJET_PONG/TP_PONG_V4/server/main.js
+++ PROJET_PONG/TP_PONG_V4/server/main.js
@@ -13,15 +13,21 @@ let players = new Map();
 io.on('connection', socket => {
 	const sId = socket.id;
 	const nb = players.size + 1;
+	console.log(`Connected player #${nb} at ${socket.id}`);
 	if (nb < 3) {
 		players.set(nb, socket.id);
 		socket.on( 'disconnect', () => {
 			players.delete(nb);
+			console.log(`Disconnected player #${nb} at ${socket.id}`);
 		});
+	}
+	else {
+		console.log(`Disconnecting ${socket.id} : too many players already connected`);
+		socket.disconnect(true);
 	}
 	socket.emit('number', nb);
 	console.log(`Sent ${nb} at ${socket.id}`);
 });
 
 
-server.listen(2800);
+server.listen(8000);
