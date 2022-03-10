05692ae65d04e2c2178ed836563ea509b947bce8
Alexandre Ledun <alexandre.ledun.etu@univ-lille.fr>
2022-03-10 11:47:53 +0100

    dossier server/dossier client & installation npm mongoose (dans server pour l'instant?)

--- vendez_les_votres/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET home page. */
-// 'render' utilise le moteur de vue (donc pour nous, 'pug')
-// pour construire les pages fournies en réponse
-router.get('/', (req, res, next) => res.render('index', { title: 'Express' }) );
-
-module.exports = router;
