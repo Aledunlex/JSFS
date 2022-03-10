359eb1e5d30598fbc3ecc7fb7ec849480a8c7d15
Alexandre Ledun <alexandre.ledun.etu@univ-lille.fr>
2022-03-10 11:57:56 +0100

    creation de client/model/User.js et Item.js, à voir si ça va vraiment dans client

---
+++ vendez_les_votres/client/model/Item.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    soldBy: {
+        type: User, //jsp si ça se fait
+        required: true
+    },
+    price: {
+        type: int,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('item', itemSchema);
